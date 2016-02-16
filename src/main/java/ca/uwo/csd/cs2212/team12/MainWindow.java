a431183a45f0713458cab6517ab68fbada2a0fe3
Anthony Giugno <agiugno2@uwo.ca>
2016-02-16 13:51:39 -0500

    Added LoginPanel, added presentPanel(..) method to MainWindow.

--- src/main/java/ca/uwo/csd/cs2212/team12/MainWindow.java
+++ src/main/java/ca/uwo/csd/cs2212/team12/MainWindow.java
@@ -1,5 +1,6 @@
 package ca.uwo.csd.cs2212.team12;
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
     private JTextField txtName;
@@ -14,9 +15,18 @@ public class MainWindow extends JFrame {
 
     private void initUI() {
         this.setTitle("StayFit");
-        this.setSize(800, 300);
+        this.setSize(950, 600);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.setLayout(new BorderLayout());
+        this.add(new LoginPanel(), BorderLayout.CENTER);
+    }
+
+    /* Replaces the panel currently being shown in MainWindow and replaces it with a new one. */
+    public void presentPanel(JPanel panel) {
+        this.removeAll();
+        this.add(panel, BorderLayout.CENTER);
     }
 
 }
