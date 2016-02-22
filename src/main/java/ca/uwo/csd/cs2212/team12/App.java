ec626c563751e5c4a05bd423ff05b35a9780b933
Elijah Ward <eward24@uwo.ca>
2016-02-21 23:02:34 -0500

    Revert "Merge branch 'feature/W162212T12-63-1st-version-of-data-model-classes' of ssh://repo.gaul.csd.uwo.ca:7999/cs2212_w2016/team12 into feature/W162212T12-63-1st-version-of-data-model-classes"
    This reverts commit 48c3fa31f7bc17876b68749c5ed12477e4f1d6b2, reversing
    changes made to 2ed4af8c9dc34c0e1cd52c638aa2b6f012247970.

--- src/main/java/ca/uwo/csd/cs2212/team12/App.java
+++ src/main/java/ca/uwo/csd/cs2212/team12/App.java
@@ -1,24 +1,14 @@
 package ca.uwo.csd.cs2212.team12;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import javax.swing.SwingUtilities;
-
+ import org.apache.logging.log4j.LogManager;
+ import org.apache.logging.log4j.Logger; 
 public class App {
 
-  static Logger logger = LogManager.getLogger (App.class.getName());
+      static Logger logger = LogManager.getLogger (App.class.getName());
 
   public static void main ( String[] args ) {
     logger.trace("Entering main");
     logger.warn("Hello Maven/log4j World"); 
-    logger.info("Our team name is team 12");
-
-    SwingUtilities.invokeLater(new Runnable() {
-      @Override
-      public void run() {
-        MainWindow window = new MainWindow();
-        window.setVisible(true);
-      }
-    });
+    logger.info("Our team name is team 12"); 
    logger.trace("Exiting main");  
      }
 }
