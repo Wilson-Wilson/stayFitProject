d887d318b115685ca2911c08b913c1d082418832
Elijah Ward <eward24@uwo.ca>
2016-02-25 14:31:38 -0500

    Made some updates to Accolade and DataEntry objects and the collections they are stored in.

--- src/main/java/ca/uwo/csd/cs2212/team12/AccoDict.java
+++ src/main/java/ca/uwo/csd/cs2212/team12/AccoDict.java
@@ -1,4 +1,64 @@
 
-public class AccoDict {
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
+
+public class AccoDict implements Serializable {
+
+  private List<Accolade> accoList;
+
+  private static final long serialVersionUID= 1L;
+  private static final String FILENAME= "accolades.dict";
+
+  public AccoDict(){
+    accoList= new ArrayList<Accolade>();
+  }
+
+  public boolean userHasAcco(int accoID){
+    if(this.get(accoID).userObtained == true){
+      return true;
+    }
+    return false;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //The following methods are used to persist AccoDict object between runs
+
+  private static void storeAccolades(){
+    try{
+      ObjectOutputStream out= new ObjectOutputStream( new FileOutputStream(FILENAME));
+      out.writeObject(this);
+      out.close();
+        } catch(IOException e){
+            System.out.println("Accolades could not be saved to disk. IO error occured.");
+            e.printStackTrace();
+          }
+
+    }
+
+
+  private static void loadAccolades(){
+    try{
+      ObjectInputStream in= new ObjectInputStream( new FileInputStream(FILENAME));
+      DataEntry data= (DataEntry) in.readObject();
+
+      in.close();
+        } catch (IOException e){
+            System.out.println("Accolades could not be loaded from disk. IO error occured.");
+            e.printStackTrace;
+          }
+    }
 
 }
--- src/main/java/ca/uwo/csd/cs2212/team12/UserInfo.java
+++ src/main/java/ca/uwo/csd/cs2212/team12/UserInfo.java
@@ -1,12 +1,15 @@
 
 public class UserInfo {
 
+	private String user;
+
+
 	private int calBurnedDay;
 	private int calBurnedWeek;
 	private int calBurnedMonth;
 	private int calBurnedYear;
 	private int calBurnedLife;
-	
+
 	private int distanceDay;
-	
+
 }
