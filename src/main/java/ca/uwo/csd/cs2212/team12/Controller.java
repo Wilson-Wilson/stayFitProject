77ab539a6a9de630d1a3adc967e0e94088a60c43
Leo Kim <lkim68@uwo.ca>
2016-03-16 22:15:13 -0400

    Added day, month and week total calculating methods

--- src/main/java/ca/uwo/csd/cs2212/team12/Controller.java
+++ src/main/java/ca/uwo/csd/cs2212/team12/Controller.java
@@ -20,27 +20,34 @@ public class Controller {
 	
 	public static void changeDate(String newer, String older){
 		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		LocalDate newDate = LocalDate.parse(newer, formatter);
 		LocalDate oldDate = LocalDate.parse(older, formatter);
 		
 		if (!isWithinRange(newDate)){
 			
 			// update day data
-			DataEntry  theDay = theDictionary.getDictionary();
-			/* UI.setCaloriesVariable(newDate.getCalories) ;
-			...
+			int [] dayValues = getDayData(newDate); 
+			/* 
+			 * UI.setCaloriesVariable(dayValues[0]);
 			*/
 			
-			//update week data
-			if(isSameWeek(newDate, oldDate)){
-				
+			//update week data if different week
+			if(!isSameWeek(newDate, oldDate)){
+				int[] weekValues = getWeekData(newDate);
+				/*UI.setCaloriesVariable(weekValues[0]);
+				...
+				*/
 			}
 			
-			//update month data
-			if(isSameMonth(newDate, oldDate){
-				
+			//update month data if different month
+			if(!isSameMonth(newDate, oldDate)){
+				int[] monthValues = getMonthData(newDate);
+				/*UI.setCaloriesVariable(weekValues[0]);
+				...
+				*/
 			}
+
 			
 			//update data from UserInfo class
 		}
@@ -53,7 +60,6 @@ public class Controller {
 	 * Application starts up w internet
 	 * 
 	 * User presses refresh
-	 * User changes date
 	 * Application closes
 	 */
 	
@@ -62,7 +68,6 @@ public class Controller {
 	 * When will the application require the local data?
 	 * 
 	 * Application starts up without internet
-	 * User changes date
 	 */
 	
 	/**
@@ -75,11 +80,102 @@ public class Controller {
 	 * 
 	 * 
 	 */
+	private static int[] getDayData(LocalDate theDate){
+		
+		int [] dayValues = new int[6];
+		
+		String dayString = theDate.toString();
+		DataEntry theDay = theDictionary.getDictionary().get(dayString);
+		
+		dayValues[0] = theDay.getCalBurned();
+		dayValues[1] = theDay.getDistanceTravelled();
+		dayValues[2] = theDay.getFloorsClimbed();
+		dayValues[3] = theDay.getStepsTaken();
+		dayValues[4] = theDay.getActMins();
+		dayValues[5] = theDay.getSedMins();
+		
+		return dayValues;
+		
+	}
 	
+	private static int[] getWeekData(LocalDate theDate){
+		
+		LocalDate dayObject;
+		String dayString;
+		DataEntry currentDay;
+		int i;
+		int [] weekValues = new int[6];
+		
+		//Find the Sunday in the same week as the newer date
+		switch(theDate.getDayOfWeek()){
+			case SUNDAY:	dayObject = theDate;
+							break;
+			case MONDAY:	dayObject = theDate.minusDays(1);
+			 			 	break;
+			case TUESDAY:	dayObject = theDate.minusDays(2);
+			 			 	break;
+			case WEDNESDAY:	dayObject = theDate.minusDays(3);
+			 				break;
+			case THURSDAY:	dayObject = theDate.minusDays(4);
+							break;	
+			case FRIDAY:	dayObject = theDate.minusDays(5);
+							break;
+			case SATURDAY:	dayObject = theDate.minusDays(6);
+							break;
+			default:		break;
+		}
+		
+		dayString = dayObject.toString();
+		
+		for (i = 0; i < 7; i++){
+			
+			currentDay = theDictionary.getDictionary.get(dayString);
+			
+			weekValues[0] += currentDay.getCalBurned();
+			weekValues[1] += currentDay.getDistanceTravelled();
+			weekValues[2] += currentDay.getFloorsClimbed();
+			weekValues[3] += currentDay.getStepsTaken();
+			weekValues[4] += currentDay.getActMins();
+			weekValues[5] += currentDay.getSedMins();
+			
+			dayObject.plusDays(1);
+			dayString = dayObject.toString();
+			
+		}
+		return weekValues;
+	}
+	
+	private static int[] getMonthData(LocalDate theDate){
+		
+		LocalDate dayObject;
+		String dayString;
+		DataEntry currentDay;
+		int [] monthValues = new int[6];
+		int currentMonth = theDate.getMonthValue();
+		
+		//get first day of the month
+		dayObject = theDate.withDayOfMonth(1);
+		
+		while(dayObject.getMonthValue()==currentMonth){
+			
+			currentDay = theDictionary.getDictionary.get(dayString);
+			
+			monthValues[0] += currentDay.getCalBurned();
+			monthValues[1] += currentDay.getDistanceTravelled();
+			monthValues[2] += currentDay.getFloorsClimbed();
+			monthValues[3] += currentDay.getStepsTaken();
+			monthValues[4] += currentDay.getActMins();
+			monthValues[5] += currentDay.getSedMins();
+			
+			dayObject.plusDays(1);
+			dayString = dayObject.toString();
+			
+		}
+	}
 	
 	private static boolean isWithinRange(LocalDate theDate){
 		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		
     	LocalDate earliest = LocalDate.parse(theDictionary.getEarliest(), formatter);
     	LocalDate latest = LocalDate.parse(theDictionary.getLatest(), formatter);
@@ -97,8 +193,7 @@ public class Controller {
 	private static boolean isSameWeek(LocalDate newer, LocalDate older){
 		//A week is defined as Sunday - Saturday
 		
-		LocalDate startOfWeek;
-		LocalDate endOfWeek;
+		LocalDate startOfWeek, endOfWeek;
 		
 		//Define the Sunday and Saturday corresponding to the week of the older date
 		switch(older.getDayOfWeek()){
