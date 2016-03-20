ee8a0ad1d9eb10afa4091c26d9e418f04abb9238
Christopher Brown <cbrow247@uwo.ca>
2016-03-20 16:06:41 -0400

    Reformatted ImageClass

--- src/main/java/ca/uwo/csd/cs2212/team12/ImageClass.java
+++ src/main/java/ca/uwo/csd/cs2212/team12/ImageClass.java
@@ -2,102 +2,47 @@ package ca.uwo.csd.cs2212.team12;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
-
 public class ImageClass {
-	    static ImageIcon backImage = new ImageIcon("../src/main/resources/background.jpg");
-	    static ImageIcon caloriesIcon = new ImageIcon("../src/main/resources/FireIcon2.png");
-	    static ImageIcon minutesIcon = new ImageIcon("../src/main/resources/TimeIcon2.png");
-	    static ImageIcon movementIcon = new ImageIcon("../src/main/resources/RunningIcon2.png");
-	    static ImageIcon lifetimeIcon = new ImageIcon("../src/main/resources/StarIcon2.png");
-	    static ImageIcon timeseriesIcon = new ImageIcon("../src/main/resources/BarGraphIcon2.png");
-	    static ImageIcon bestdayIcon = new ImageIcon("../src/main/resources/BlueStarIcon2.png");
-	    static ImageIcon exitIcon = new ImageIcon("../src/main/resources/closebutton2.png");
-	    static ImageIcon rightarrowIcon = new ImageIcon("../src/main/resources/rightArrow2.png");
-	    static ImageIcon leftarrowIcon = new ImageIcon("../src/main/resources/leftArrow2.png");
-	    static ImageIcon trophyIcon = new ImageIcon("../src/main/resources/Trophy.png");
-	    static ImageIcon cardIcon =new ImageIcon("../src/main/resources/dash.png");
-	   	static ImageIcon settingsIcon = new ImageIcon("../src/main/resources/Gear-icon2.png");
-	   	static ImageIcon trophyIcon2=new ImageIcon("../src/main/resources/Trophy2.png");
-	   	static ImageIcon smallrightarrow=new ImageIcon("../src/main/resources/smallrightarrow.png");
-		static ImageIcon homebuttonIcon= new ImageIcon("../src/main/resources/home2.png");
-	    static ImageIcon smallleftarrow= new ImageIcon("../src/main/resources/smallleftarrow.png");
-	    static ImageIcon refreshIcon = new ImageIcon("../src/main/resources/refreshIcon.png");
-	    static ImageIcon calendarIcon= new ImageIcon("../src/main/resources/calendarIcon.png");
-	    
-	    static public ImageIcon getBackImage() {
-			return backImage;
-		}
-	
-		static public ImageIcon getCaloriesIcon() {
-			return caloriesIcon;
-		}
-		
-		static public ImageIcon getMinutesIcon() {
-			return minutesIcon;
-		}
-		
-		static public ImageIcon getMovementIcon() {
-			return movementIcon;
-		}
-		
-		static  ImageIcon getLifetimeIcon() {
-			return lifetimeIcon;
-		}
-		
-		static public ImageIcon getTimeseriesIcon() {
-			return timeseriesIcon;
-		}
-		
-		static public ImageIcon getBestdayIcon() {
-			return bestdayIcon;
-		}
-		
-		static public ImageIcon getExitIcon() {
-			return exitIcon;
-		}
-		
-		static public ImageIcon getRightarrowIcon() {
-			return rightarrowIcon;
-		}
-		
-		static public ImageIcon getLeftarrowIcon() {
-			return leftarrowIcon;
-		}
-		
-		static public ImageIcon getTrophyIcon() {
-			return trophyIcon;
-		}
-		
-		static public ImageIcon getCardIcon() {
-			return cardIcon;
-		}
-		static public ImageIcon getSettingsIcon() {
-				return settingsIcon;
-			}
-	 	public static ImageIcon getTrophyIcon2() {
-			return trophyIcon2;
-		}
-	 
-		public static ImageIcon getSmallrightarrow() {
-			return smallrightarrow;
-			
-		}
-		public static ImageIcon getCalendarIcon(){
-			return calendarIcon;
-		}
-		public static ImageIcon getRefreshIcon(){
-			return refreshIcon;
-		}
-		
 
-		public static ImageIcon getHomebuttonIcon() {
-			return homebuttonIcon;
-		}
+	// Image files
+	static ImageIcon backImage 			= new ImageIcon("../src/main/resources/background.jpg");
+	static ImageIcon caloriesIcon 		= new ImageIcon("../src/main/resources/FireIcon2.png");
+	static ImageIcon minutesIcon 		= new ImageIcon("../src/main/resources/TimeIcon2.png");
+	static ImageIcon movementIcon 		= new ImageIcon("../src/main/resources/RunningIcon2.png");
+	static ImageIcon lifetimeIcon 		= new ImageIcon("../src/main/resources/StarIcon2.png");
+	static ImageIcon timeseriesIcon 	= new ImageIcon("../src/main/resources/BarGraphIcon2.png");
+	static ImageIcon bestdayIcon 		= new ImageIcon("../src/main/resources/BlueStarIcon2.png");
+	static ImageIcon exitIcon 			= new ImageIcon("../src/main/resources/closebutton2.png");
+	static ImageIcon rightarrowIcon 	= new ImageIcon("../src/main/resources/rightArrow2.png");
+	static ImageIcon leftarrowIcon 		= new ImageIcon("../src/main/resources/leftArrow2.png");
+	static ImageIcon trophyIcon 		= new ImageIcon("../src/main/resources/Trophy.png");
+	static ImageIcon cardIcon 			= new ImageIcon("../src/main/resources/dash.png");
+	static ImageIcon settingsIcon 		= new ImageIcon("../src/main/resources/Gear-icon2.png");
+	static ImageIcon trophyIcon2		= new ImageIcon("../src/main/resources/Trophy2.png");
+	static ImageIcon smallrightarrow	= new ImageIcon("../src/main/resources/smallrightarrow.png");
+	static ImageIcon homebuttonIcon		= new ImageIcon("../src/main/resources/home2.png");
+	static ImageIcon smallleftarrow		= new ImageIcon("../src/main/resources/smallleftarrow.png");
+	static ImageIcon refreshIcon 		= new ImageIcon("../src/main/resources/refreshIcon.png");
+	static ImageIcon calendarIcon		= new ImageIcon("../src/main/resources/calendarIcon.png");
 
-		public static ImageIcon getSmallleftarrow() {
-			return smallleftarrow;
-		}
-
-		
-			
+	// Methods to retrieve image files
+	public static ImageIcon getBackImage() 			{return backImage;}
+	public static ImageIcon getCaloriesIcon() 		{return caloriesIcon;}
+	public static ImageIcon getMinutesIcon() 		{return minutesIcon;}
+	public static ImageIcon getMovementIcon() 		{return movementIcon;}
+	public static ImageIcon getLifetimeIcon() 		{return lifetimeIcon;}
+	public static ImageIcon getTimeseriesIcon() 	{return timeseriesIcon;}
+	public static ImageIcon getBestdayIcon() 		{return bestdayIcon;}
+	public static ImageIcon getExitIcon() 			{return exitIcon;}
+	public static ImageIcon getRightarrowIcon() 	{return rightarrowIcon;}
+	public static ImageIcon getLeftarrowIcon() 		{return leftarrowIcon;}
+	public static ImageIcon getTrophyIcon() 		{return trophyIcon;}
+	public static ImageIcon getCardIcon() 			{return cardIcon;}
+	public static ImageIcon getSettingsIcon() 		{return settingsIcon;}
+	public static ImageIcon getTrophyIcon2() 		{return trophyIcon2;}
+	public static ImageIcon getSmallrightarrow() 	{return smallrightarrow;}
+	public static ImageIcon getCalendarIcon()		{return calendarIcon;}
+	public static ImageIcon getRefreshIcon()		{return refreshIcon;}
+	public static ImageIcon getHomebuttonIcon() 	{return homebuttonIcon;}
+	public static ImageIcon getSmallleftarrow() 	{return smallleftarrow;}
 }
