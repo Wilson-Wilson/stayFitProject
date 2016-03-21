2f6e58eeaf7a781b953db0b3c0a1621f7b82cc1f
Jonathan Wilson <jwils269@uwo.ca>
2016-03-21 11:46:13 -0400

    reconciled API and RealAPI; made path names relative

--- src/main/java/ca/uwo/csd/cs2212/team12/API.java
+++ src/main/java/ca/uwo/csd/cs2212/team12/API.java
@@ -12,75 +12,78 @@ public interface API {
 
     /**
      * This method is used to fetch the number of calories burned.
-     * @return int This returns the number of calories burned.
+     * @return String This returns the number of calories burned.
      */
-    public int getCalBurned();
+    public String getCalBurned();
 
     /**
      * This method is used to fetch the number of active minutes.
-     * @return int This returns the number of active minutes.
+     * @return String This returns the number of active minutes.
      */
-    public int getActiveMinutes();
+    public String getActiveMinutes();
 
     /**
      * This method is used to fetch the number of sedentary minutes.
-     * @return int This returns the number of sedentary minutes.
+     * @return String This returns the number of sedentary minutes.
      */
-    public  int getSedentaryMinutes();
+    public  String getSedentaryMinutes();
 
     /**
      * This method is used to fetch the distance travelled.
-     * @return int This returns the distance travelled.
+     * @return String This returns the distance travelled.
      */
-    public  int getDistance();
+    public  String getDistance();
 
     /**
      * This method is used to fetch the number of floors climbed.
-     * @return int This returns the number of floors climbed.
+     * @return String This returns the number of floors climbed.
      */
-    public  int getFloors();
+    public  String getFloors();
 
     /**
      * This method is used to fetch the number of steps.
-     * @return int This returns the number of steps.
+     * @return String This returns the number of steps.
      */
-    public  int getSteps();
+    public  String getSteps();
 
     /**
      * This method is used to fetch the number of floors climbed in total.
-     * @return int This returns the number of floors climbed in total.
+     * @return String This returns the number of floors climbed in total.
      */
-    public  int getLifeTimeFloors();
+    public  String getLifeTime();
 
     /**
      * This method is used to fetch the number of steps total.
-     * @return int This returns the number of steps total.
+     * @return String This returns the number of steps total.
      */
-    public  int getLifetimeSteps();
+    public String getCalSeries();
 
     /**
      * This method is used to fetch the total distance travelled.
-     * @return int This returns the total distance travelled.
+     * @return String This returns the total distance travelled.
      */
-    public  int getLifetimeDistance();
+    public  String getStepsSeries();
 
     /**
      * This method is used to fetch the best number of floors climbed.
-     * @return int This returns the best number of floors climbed.
+     * @return String This returns the best number of floors climbed.
      */
-    public  int getBestFloors();
+    public  String getSedentaryMinutesSeries();
 
     /**
      * This method is used to fetch the best number of steps.
-     * @return int This returns the best number of steps.
+     * @return String This returns the best number of steps.
      */
-    public  int getBestSteps();
+    public  String getFloorsSeries();
 
     /**
      * This method is used to fetch the best distance travelled.
-     * @return int This returns the best distance travelled.
+     * @return String This returns the best distance travelled.
      */
-    public  int getBestDistance();
+    public  String getActiveMinutesSeries();
+    
+    
+    public String getDistanceSeries();
 
 
 }
