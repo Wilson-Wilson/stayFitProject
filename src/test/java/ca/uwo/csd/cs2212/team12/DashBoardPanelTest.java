146b519e08dc685a24ac3a725bc0674a571f201a
Christopher Brown <cbrow247@uwo.ca>
2016-03-23 01:15:42 -0400

    Removed unnecessary tests and completed DashBoardPanel test

--- src/test/java/ca/uwo/csd/cs2212/team12/DashBoardPanelTest.java
+++ src/test/java/ca/uwo/csd/cs2212/team12/DashBoardPanelTest.java
@@ -1,34 +1,29 @@
 package ca.uwo.csd.cs2212.team12;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by Chris on 2016-03-22.
- */
 public class DashBoardPanelTest {
+
+    private DashBoardPanel pan;
 
     @org.junit.Before
     public void setUp() throws Exception {
-
-    }
-
-    @org.junit.After
-    public void tearDown() throws Exception {
-
+        pan = new DashBoardPanel();
     }
 
     @org.junit.Test
     public void testSubtractDay() throws Exception {
-
+        Date day = new Date(2000000000);
+        day = pan.addDay(day);
+        assertEquals(day,new Date(1000000000));
     }
 
     @org.junit.Test
     public void testAddDay() throws Exception {
-
+        Date day = new Date(1000000000);
+        day = pan.addDay(day);
+        assertEquals(day,new Date(2000000000));
     }
 
-    @org.junit.Test
-    public void testCheckPref() throws Exception {
-
-    }
 }
