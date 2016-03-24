98aa1745db892141bc3d9f90de62cc7b25e1d3fc
Leo Kim <lkim68@uwo.ca>
2016-03-24 19:46:10 -0400

    Incomplete Controller Test

--- src/test/java/ca/uwo/csd/cs2212/team12/ControllerTest.java
+++ src/test/java/ca/uwo/csd/cs2212/team12/ControllerTest.java
@@ -1,5 +1,8 @@
 package ca.uwo.csd.cs2212.team12;
 import java.text.ParseException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.json.JSONException;
 import org.junit.After;
@@ -15,48 +18,34 @@ public class ControllerTest {
 	TimeSeriesData testTimeSeries;
 	
 	@Test
-	public void testOnStartUp() throws JSONException, ParseException {
+	public void mainTest() throws JSONException, ParseException, IOException {
 		
-		//Test case1: no serialized data to restore and Internet is available
+		//Test case1: no serialized data and internet is available:
+		Files.deleteIfExists(Paths.get("datadict.boop"));
+		Files.deleteIfExists(Paths.get("timeseries.boop"));
+		Files.deleteIfExists(Paths.get("userinfo.boop"));
+		Files.deleteIfExists(Paths.get("preferences.boop"));
+		Files.deleteIfExists(Paths.get("dailygoals.boop"));
 		Controller.onStartUp();
 		
-		//Assert that default Preference constructor was called successfully
-		testPref = Controller.getPreferences();
+			//Assert that default Preference constructor was called successfully
 		Assert.assertEquals(true, Preferences.isShowBestDaysCard());
 		Assert.assertEquals(true, Preferences.isShowCaloriesCard());
 		Assert.assertEquals(true, Preferences.isShowMinutesCard());
 		Assert.assertEquals(true, Preferences.isShowMovementsCard());
 		Assert.assertEquals(true, Preferences.isShowLifetimeCard());
 		Assert.assertEquals(true, Preferences.isShowBestDaysCard());
+			//Modify Preferences for serialization purposes
 		
-		//Assert that today's data was properly called into TimeSeriesData 
-		testTimeSeries = Controller.getTimeSeries()
-		Assert.assertEquals(true, TimeSeriesData.getCaloriesSet.));
+		Controller.onClose();
+		System.exit(2);
 		
+		//Test case2: serialized data is available but internet is not available
+		Controller.onStartUp();
+		System.exit(2);
 	      
 	}
 	
-	@Before
-	public void tearDown() throws Exception{
-		
-		//create Serializable objects
-	}
-	
-	@Test
-	public void testChangeDate() throws Exception {
-
-	}
-	
-	@Test
-	public void testOnClose() throws Exception {
-		Controller.onClose();
-
-	}
-
-	@Test
-	public void testOnStartUp1() throws Exception {
-
-	}
 
 
 }
